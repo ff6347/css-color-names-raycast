ac652546f597cc0cd6600945cd6adfd5296d7969
ff6347 <hash@ff6347.email>
2024-09-16 11:13:48 +0200

    fix: Error in helper script

--- src/scripts/create-color-images.ts
+++ src/scripts/create-color-images.ts
@@ -1,7 +1,7 @@
 
 import { colors } from '../colors';
 import { exec } from 'child_process';
-colors.items.forEach(element => {
+colors().items.forEach(element => {
   exec(`magick -size 256x256 xc:${element.arg} ../../assets/${element.arg}.png`, (error, stdout, stderr) => {
     if (error) {
 			console.error(error);
