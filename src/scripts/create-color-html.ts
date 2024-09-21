be9145ac0f90086665cf16bdc482e13b6b343efb
ff6347 <hash@ff6347.email>
2024-09-21 11:39:01 +0200

    chore: Some dev overviews

---
+++ src/scripts/create-color-html.ts
@@ -0,0 +1,40 @@
+import { colors } from "../colors";
+import fs from "fs";
+import path from "path";
+
+const html = colors()
+  .items.map(
+    (element) =>
+      `<div class="item" style="background-color: ${element.arg}; width: 100px; height: 100px;"><span class="txt">${element.arg}</span></div>`,
+  )
+  .join("\n");
+const document = /*html */ `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+		<style>
+			main{
+				display: grid;
+				grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+				}
+				.item{
+					display: flex;
+					justify-content: start;
+					align-items: start;
+					color: white;
+					font-size: 10px;
+				}
+				.txt{background-color: black; padding: 5px;}
+		</style>
+	</head>
+	<body>
+		<main>${html}</main>
+	</body>
+	</html>
+	`;
+console.log(document);
+
+fs.writeFileSync(path.resolve(__dirname, "../../assets/colors.html"), document);
